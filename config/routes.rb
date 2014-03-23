1980b52b6de6e721557d4e8907fb83a1e3c8c459
Janice Smith <janice.k.n.smith@gmail.com>
2014-03-23 15:40:56 -0400

    created scores table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PlayPeriodically::Application.routes.draw do
+  resources :scores
+
   devise_for :users
   root to: 'home#index'
 end
---
+++ db/migrate/20140323193229_create_scores.rb
@@ -0,0 +1,10 @@
+class CreateScores < ActiveRecord::Migration
+  def change
+    create_table :scores do |t|
+      t.integer :value
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
